646718da02ad17845555eb3cd3bbedd0872bbc26
Licoli Santos <joaogabriellicoli@gmail.com>
2025-11-01 00:44:26 -0300

    Lacos de Repeticao

--- lacosRepeticao/repeticaoFor.js
+++ lacosRepeticao/repeticaoFor.js
@@ -2,16 +2,16 @@
 const leia = require('readline-sync');
 
 // Entrada de dados pelo usuário
-let num1 = leia.questionInt("Digite o primeiro numero do intervalo: ");
+let num1 = leia.questionInt("Digite o primeiro numero do intervalo: "); // questionInt() leitura numeros inteiros do teclado.
 let num2 = leia.questionInt("Digite o ultimo numero do intervalo: ");
 
-// Verificando se o intervalo é válido / uso de if else
-if (num1 >= num2) {
+// Verificando se o intervalo é válido / uso de if e else
+if (num1 >= num2) { // impede que o primeiro número seja maior ou igual ao segundo
     console.log("Intervalo invalido!");
 } else {
     console.log(`No intervalo entre ${num1} e ${num2}:`);
 
-    // Estrutura de repetição FOR
+    // Estrutura de repetição FOR //
     for (let i = num1; i <= num2; i++) {
         if (i % 3 === 0 && i % 5 === 0) {
             console.log(`${i} e multiplo de 3 e 5`);
